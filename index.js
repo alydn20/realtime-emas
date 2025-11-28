e635f0c25eb25bf1ebba1d1a36ccf6e92557c4a6
alydn62 <muhamadaliyudin62@gmail.com>
2025-11-28 08:51:41 +0700

    Remove clock from top-right header (keep only below Sound)

--- index.js
+++ index.js
@@ -5914,8 +5914,6 @@ app.get('/monitoring', async (_req, res) => {
         <div class="subtitle">Real-time Treasury Gold Rates</div>
       </div>
       <div class="header-right">
-        <div class="clock" id="clock">--:--:--</div>
-        <div class="date-info" id="dateInfo">Loading...</div>
         <button class="logout-btn" id="logoutBtn" onclick="logout()" title="Logout">
           <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="16" height="16"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/><polyline points="16 17 21 12 16 7"/><line x1="21" y1="12" x2="9" y2="12"/></svg>
         </button>
